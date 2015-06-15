122fdbe16c3e6ef9b348d9a0c9941b5285bdd546
SuperDanny <jameslv@163.com>
2015-06-15 23:20:08 +0800

    完善自定义cell

---
+++ SchoolMate/Utility/View/Other/commentItemView.m
@@ -0,0 +1,21 @@
+//
+//  commentItemView.m
+//  SchoolMate
+//
+//  Created by SuperDanny on 15/6/15.
+//  Copyright (c) 2015年 libiwu. All rights reserved.
+//
+
+#import "commentItemView.h"
+
+@implementation commentItemView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
